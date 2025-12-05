fa618e9d0afa23453be2f6358418fbf5672498ac
Norbert Horvath <norbert.horvath@ni.com>
2025-12-05 15:23:38 +0100

    Implemented basic circle handling

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,48 @@
 #include <GLFW/glfw3.h>
 #include <fmt/core.h>
 #include <glm/ext/matrix_clip_space.hpp>
+#include <glm/geometric.hpp>
 #include <glm/glm.hpp>
 #include <glm/gtc/constants.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <vector>
 
+#define MAX_CIRCLE_SIZE 150.0f
+#define MIN_CIRCLE_SIZE 5.0f
+
 float window_width = 800;
 float window_height = 600;
 
-struct Circle
+glm::vec2 mouse_position = glm::vec2(0.0f);
+
+int holded_circle_index = -1;
+
+class Circle
 {
+public:
     float radius;
     glm::vec2 position;
+    std::vector<glm::vec2> vertices;
+    Circle(float r, glm::vec2 pos)
+    {
+        this->position = pos;
+        this->radius = r;
+
+        vertices = std::vector<glm::vec2>();
+
+        auto alpha = 2 * glm::pi<float>() / 100.0f;
+
+        for (int i = 0; i < 100; i++)
+        {
+            vertices.push_back(glm::vec2(0.0f));
+            vertices.push_back(glm::vec2(glm::sin(alpha * (i + 1)), glm::cos(alpha * (i + 1))));
+            vertices.push_back(glm::vec2(glm::sin(alpha * i), glm::cos(alpha * i)));
+        }
+    }
 };
+
+std::vector<Circle> circles;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
@@ -25,11 +53,82 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     glViewport(0, 0, width, height);
 }
 
+void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    mouse_position = glm::vec2(xpos, ypos);
+
+    if (holded_circle_index != -1)
+    {
+        circles[holded_circle_index].position = mouse_position;
+    }
+}
+
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+    {
+        for (auto i = 0; i < circles.size(); i++)
+        {
+            auto circle = circles[i];
+
+            if (glm::distance(mouse_position, circle.position) <= circle.radius)
+            {
+                holded_circle_index = i;
+                break;
+            }
+        }
+
+        if (holded_circle_index == -1)
+        {
+            circles.push_back(Circle(50.0f, mouse_position));
+        }
+        return;
+    }
+
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE)
+    {
+        holded_circle_index = -1;
+        return;
+    }
+
+    if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
+    {
+        for (auto i = 0; i < circles.size(); i++)
+        {
+            auto circle = circles[i];
+
+            if (glm::distance(mouse_position, circle.position) <= circle.radius)
+            {
+                circles.erase(circles.begin() + i);
+                break;
+            }
+        }
+        return;
+    }
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    if (holded_circle_index != -1)
+    {
+        circles[holded_circle_index].radius += yoffset;
+
+        if (circles[holded_circle_index].radius < MIN_CIRCLE_SIZE)
+        {
+            circles[holded_circle_index].radius = MIN_CIRCLE_SIZE;
+        }
+        else if (circles[holded_circle_index].radius > MAX_CIRCLE_SIZE)
+        {
+            circles[holded_circle_index].radius = MAX_CIRCLE_SIZE;
+        }
+    }
+}
+
 GLFWwindow* initialize()
 {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     auto window = glfwCreateWindow(window_width, window_height, "Circle skinning", NULL, NULL);
@@ -96,30 +195,25 @@ unsigned int get_shader_program()
     return shader_program;
 }
 
-void render_circles(unsigned int vbo, unsigned int vao, std::vector<Circle> circles)
+void render_circles(unsigned int vbo, unsigned int vao, std::vector<Circle> circles, GLint model_uniform)
 {
-    auto vertices = std::vector<glm::vec3>();
-
-    auto alpha = 2 * glm::pi<float>() / 100.0f;
-
     for (auto circle : circles)
     {
-        for (int i = 0; i < 100; i++)
-        {
-            vertices.push_back(glm::vec3(circle.position.x, circle.position.y, 1));
-            vertices.push_back(glm::vec3(circle.position.x + circle.radius * glm::sin(alpha * (i + 1)), circle.position.y +  circle.radius * glm::cos(alpha * (i + 1)), 1));
-            vertices.push_back(glm::vec3(circle.position.x + circle.radius * glm::sin(alpha * i), circle.position.y + circle.radius * glm::cos(alpha * i), 1));
-        }
+        auto model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(circle.position, 0.0f));
+        model = glm::scale(model, glm::vec3(circle.radius, circle.radius, 1.0f));
+
+        glUniformMatrix4fv(model_uniform, 1, GL_FALSE, glm::value_ptr(model));
+
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, circle.vertices.size() * sizeof(circle.vertices[0]), &circle.vertices[0], GL_STATIC_DRAW);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+        glBindVertexArray(vao);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glDrawArrays(GL_TRIANGLES, 0, circle.vertices.size());
+        glBindVertexArray(0);
     }
-
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(vertices[0]), &vertices[0], GL_STATIC_DRAW);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    glBindVertexArray(vao);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glDrawArrays(GL_TRIANGLES, 0, vertices.size());
-    glBindVertexArray(0);
 }
 
 int main()
@@ -134,13 +228,16 @@ int main()
 
     glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, cursor_position_callback);
+    glfwSetMouseButtonCallback(window, mouse_button_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
     auto shader_program = get_shader_program();
 
     auto model_uniform = glGetUniformLocation(shader_program, "model");
     auto projection_uniform = glGetUniformLocation(shader_program, "projection");
-
-    auto model = glm::mat4(1);
 
     unsigned int circle_vbo;
     unsigned int circle_vao;
@@ -150,21 +247,10 @@ int main()
 
     glBindVertexArray(circle_vao);
     glBindBuffer(GL_ARRAY_BUFFER, circle_vbo);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0); 
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
 
-    std::vector<Circle> circles = {
-        Circle
-        {
-            .radius = .1,
-            .position = glm::vec2(0, 0),
-        },
-        Circle
-        {
-            .radius = .2,
-            .position = glm::vec2(2, 2),
-        }
-    };
+    circles = std::vector<Circle>();
 
     while(!glfwWindowShouldClose(window))
     {
@@ -174,10 +260,9 @@ int main()
 
         auto projection = glm::ortho(0.0f, window_width, window_height, 0.0f, -1.0f, 1.0f);
 
-        glUniformMatrix4fv(model_uniform, 1, GL_FALSE, glm::value_ptr(model));
         glUniformMatrix4fv(projection_uniform, 1, GL_FALSE, glm::value_ptr(projection));
 
-        render_circles(circle_vbo, circle_vao, circles);
+        render_circles(circle_vbo, circle_vao, circles, model_uniform);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
